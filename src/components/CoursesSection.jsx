24debaa0af6f501270c21f64cc07dc24e3e6a2f1
Asim <asimokon@yahoo.com>
2025-11-25 17:06:21 +0100

    edited the courses amt

--- src/components/CoursesSection.jsx
+++ src/components/CoursesSection.jsx
@@ -28,7 +28,7 @@ const CoursesSection = () => {
     {
       title: "Advanced Driving Lessons",
       duration: "4 Weeks",
-      price: "$50",
+      price: "NGN 60,000",
       features: [
         "20 Hours Behind the Wheel",
         "All Road Type Training",
